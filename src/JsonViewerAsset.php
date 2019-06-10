f7e4a8dff1c127fdf5e6d95773755465bb6c70a7
Huijie Wei <huijiewei@outlook.com>
2019-06-10 12:39:45 +0800

    初次提交

---
+++ src/JsonViewerAsset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huijiewei
+ * Date: 6/8/15
+ * Time: 12:19
+ */
+
+namespace huijiewei\jsonViewer;
+
+use yii\web\AssetBundle;
+use yii\web\JqueryAsset;
+
+class JsonViewerAsset extends AssetBundle
+{
+    public $sourcePath = '@npm/jquery.json-viewer/json-viewer';
+
+    public $js = [
+        'jquery.json-viewer.js',
+    ];
+
+    public $css = [
+        'jquery.json-viewer.css',
+    ];
+
+    public $depends = [
+        JqueryAsset::class,
+    ];
+}
---
+++ src/JsonViewerWidget.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace huijiewei\jsonViewer;
+
+use yii\base\Widget;
+
+class JsonViewerWidget extends Widget
+{
+    public $data = [];
+
+    public function init()
+    {
+        parent::init();
+
+        JsonViewerAsset::register($this->getView());
+    }
+
+    public function run()
+    {
+        $js = <<<EOD
+$('#$this->id').jsonViewer($this->data);
+EOD;
+
+        $this->getView()->registerJs($js);
+
+        $html = '<pre id="' . $this->id . '"></pre>';
+
+        return $html;
+    }
+}
